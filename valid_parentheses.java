7627fb4a9ec44c918d7cb0632e6d38d75f05161d
Pavithra c <pavi251503@gamil.com>
2026-02-22 13:05:34 +0530

    Valid Parentheses

---
+++ valid_parentheses.java
@@ -0,0 +1,20 @@
+//Problem name: Valid Parentheses
+//Probelm no:20
+//Level: Easy
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack=new Stack<>();
+        for(char c:s.toCharArray()){
+            if(c=='(' || c=='{' || c=='['){
+                stack.push(c);
+            }else{
+                if(stack.isEmpty()) return false;
+                char top=stack.pop();
+                if(c==')' && top!='(') return false;
+                if(c=='}' && top!='{' ) return false;
+                if(c==']' && top!='[') return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
