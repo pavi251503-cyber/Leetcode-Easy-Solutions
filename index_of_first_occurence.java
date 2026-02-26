f7047a579cb89a6325696bb10f93af7200fa37bc
Pavithra c <pavi251503@gamil.com>
2026-02-26 20:30:26 +0530

    First occurence of string

---
+++ index_of_first_occurence.java
@@ -0,0 +1,8 @@
+//Problem name:Find the index of the first occurence of the string
+//Problem no:28
+//Level:Easy
+class Solution {
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+}
