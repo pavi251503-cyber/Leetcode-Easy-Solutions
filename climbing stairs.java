31155c1fb6450c7ff0a4a0418a99f0f65edaff2b
Pavithra c <pavi251503@gamil.com>
2026-03-15 10:39:28 +0530

    Climbung stairs

---
+++ climbing stairs.java
@@ -0,0 +1,17 @@
+//Problem name:Climbing Stairs
+//Probelm no:70
+//Level:Easy
+class Solution {
+    public int climbStairs(int n) {
+       if(n==1) return 1;
+       if(n==2) return 2;
+       int first=1;
+       int second=2;
+       for(int i=3;i<=n;i++){
+        int third=first+second;
+        first=second;
+        second=third;
+       } 
+       return second;
+    }
+}
