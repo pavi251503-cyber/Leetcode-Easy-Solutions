bc30f9b52d34c738f441a0e09eff0296867bdac7
Pavithra c <pavi251503@gamil.com>
2026-03-16 18:03:01 +0530

    Remove duplicates from sorted array

---
+++ remove_duplicates_from_sorted_array.java
@@ -0,0 +1,16 @@
+//Problem name:Remove duplicates from sorted array
+//Problem no:83
+//Level:Easy
+class Solution{
+    public ListNode deleteDuplicates(ListNode head){
+        ListNode current=head;
+        while(current!=null && current.next!=null){
+            if(current.val==current.next.val){
+                current.next=current.next.next;
+            }else {
+                current=current.next;
+            }
+        }
+        return head;
+    }
+}
