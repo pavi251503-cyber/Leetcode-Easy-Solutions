facf641356c1c98a4dd09c6b68f7dca5e9538509
Pavithra c <pavi251503@gamil.com>
2026-03-17 18:16:37 +0530

    Binary tree traversal

---
+++ binary_tree_order_traversal.java
@@ -0,0 +1,18 @@
+//Problem name:Binary tree order traversal
+//Problem no:94
+//Level:Easy
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        inorder(root, result);
+        return result;
+    }
+    
+    private void inorder(TreeNode node, List<Integer> result) {
+        if (node == null) return;
+        
+        inorder(node.left, result);
+        result.add(node.val);
+        inorder(node.right, result);
+    }
+}
