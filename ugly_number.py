bad50316995c31ee4859dd1d7083ebf2ccd0e0df
Pavithra c <pavi251503@gamil.com>
2026-07-22 18:26:27 +0530

    Ugly number

---
+++ ugly_number.py
@@ -0,0 +1,15 @@
+"""
+Problem name: Ugly number
+Problem no: 263
+Difficulty:Easy
+"""
+class Solution:
+    def isUgly(self, n: int) -> bool:
+        if n <= 0:
+            return False
+
+        for factor in [2, 3, 5]:
+            while n % factor == 0:
+                n //= factor
+
+        return n == 1
