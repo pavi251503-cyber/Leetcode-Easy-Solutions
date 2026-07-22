77fac35787584e582ca68b6d866c36f95403592b
Pavithra c <pavi251503@gamil.com>
2026-07-22 18:29:12 +0530

    Missing number

---
+++ missing_number.py
@@ -0,0 +1,12 @@
+"""
+Problem name: Missing number
+Problem no:268
+Difficulty:Easy
+"""
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+        expected_sum = n * (n + 1) // 2
+        actual_sum = sum(nums)
+        return expected_sum - actual_sum
