58855151fa8ed99cfca21769cd4e8b718eed820c
Pavithra c <pavi251503@gamil.com>
2026-07-23 18:18:50 +0530

    First bad version

---
+++ first_bad_version.py
@@ -0,0 +1,19 @@
+"""
+Problem name: First bad version
+Problem no:278
+Difficulty: Easy
+"""
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        left = 1
+        right = n
+
+        while left < right:
+            mid = left + (right - left) // 2
+
+            if isBadVersion(mid):
+                right = mid
+            else:
+                left = mid + 1
+
+        return left
