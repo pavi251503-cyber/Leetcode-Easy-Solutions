0498de2f37e492d42c2b6a5264708a9d531bb9ae
Pavithra c <pavi251503@gamil.com>
2026-07-23 18:22:44 +0530

    Move zeros

---
+++ move_zeros.py
@@ -0,0 +1,13 @@
+"""
+Problem name: Move Zeroes
+Problem no:283
+Difficulty: Easy
+"""
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        j = 0
+
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[i], nums[j] = nums[j], nums[i]
+                j += 1
