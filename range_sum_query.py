89ea3347b15851fb5b70e79160fa1d3ca7c752f0
Pavithra c <pavi251503@gamil.com>
2026-07-30 18:25:06 +0530

    Range sum query

---
+++ range_sum_query.py
@@ -0,0 +1,15 @@
+"""
+Problem name: Range sum query
+Problem no:303
+Difficulty: Easy
+"""
+class NumArray:
+
+    def __init__(self, nums):
+        self.prefix = [0]
+
+        for num in nums:
+            self.prefix.append(self.prefix[-1] + num)
+
+    def sumRange(self, left, right):
+        return self.prefix[right + 1] - self.prefix[left]
