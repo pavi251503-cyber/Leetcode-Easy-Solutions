9996432a24abe0911ba709d249ec84046ba93bbd
Pavithra c <pavi251503@gamil.com>
2026-07-30 18:31:41 +0530

    Power of three

---
+++ power_of_three.py
@@ -0,0 +1,14 @@
+"""
+Problem name:Power of three
+Problem no:326
+Difficulty:Easy
+"""
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        if n <= 0:
+            return False
+
+        while n % 3 == 0:
+            n //= 3
+
+        return n == 1
