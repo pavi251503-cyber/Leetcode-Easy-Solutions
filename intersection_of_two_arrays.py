c96618e42e64300db49fb764e4810faf9b409053
Pavithra c <pavi251503@gamil.com>
2026-08-05 18:59:32 +0530

    Intersection of two arrays

---
+++ intersection_of_two_arrays.py
@@ -0,0 +1,11 @@
+"""
+Problem name:Intersection of two arrays
+Problem no:349
+Difficulty:Easy
+"""
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        set1 = set(nums1)
+        set2 = set(nums2)
+
+        return list(set1 & set2)
